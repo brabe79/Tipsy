e24a8ae94cd01626cf57e3e0ea6599ad0ad96ff8
Brian Rabe <brian.rabe@gmail.com>
2016-08-16 18:46:06 -0500

    Initial Commit

---
+++ Tipsy/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Tipsy
+//
+//  Created by Brian Rabe on 8/16/16.
+//  Copyright © 2016 Tipsy. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
