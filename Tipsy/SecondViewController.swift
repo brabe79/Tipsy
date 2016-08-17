cf3d9832021c49de8d898d629292d672322717b3
Brian Rabe <brian.rabe@gmail.com>
2016-08-16 23:49:16 -0500

    Added Map Functionality

--- Tipsy/SecondViewController.swift
+++ Tipsy/SecondViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import MapKit
 
 class SecondViewController: UIViewController {
-
+   let locationManager = CLLocationManager()
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,3 +28,22 @@ class SecondViewController: UIViewController {
 
 }
 
+extension SecondViewController: CLLocationManagerDelegate {
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        if status == .AuthorizedWhenInUse {
+            locationManager.requestLocation()
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            let span = MKCoordinateSpanMake(0.05, 0.05)
+            let region = MKCoordinateRegion(center: location.coordinate, span: span)
+            mapView.setRegion(region, animated: true)
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print("error:: (error)")
+    }
+}
