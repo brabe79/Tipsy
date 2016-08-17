7acc4b3a2de7ba7278f03881609c28280fc1dcfa
Brian Rabe <brian.rabe@gmail.com>
2016-08-16 23:27:04 -0500

    Version 1 : Added Facebook Integration

--- Tipsy/FirstViewController.swift
+++ Tipsy/FirstViewController.swift
@@ -7,14 +7,85 @@
 //
 
 import UIKit
-
-class FirstViewController: UIViewController {
-
+import FBSDKCoreKit
+import FBSDKLoginKit
+class FirstViewController: UIViewController, FBSDKLoginButtonDelegate  {
+@IBOutlet weak var profileImage: UIImageView!
+@IBOutlet weak var lblName: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if (FBSDKAccessToken.currentAccessToken() != nil)
+        {
+            // User is already logged in, do work such as go to next view controller.
+             returnUserData()
+        }
+        else
+        {
+            let loginView : FBSDKLoginButton = FBSDKLoginButton()
+            self.view.addSubview(loginView)
+            loginView.center = self.view.center
+            loginView.readPermissions = ["public_profile", "email", "user_friends"]
+            loginView.delegate = self
+        }
+    }
+    // Facebook Delegate Methods
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        print("User Logged In")
+        
+        if ((error) != nil)
+        {
+            // Process error
+        }
+        else if result.isCancelled {
+            // Handle cancellations
+        }
+        else {
+            // If you ask for multiple permissions at once, you
+            // should check if specific permissions missing
+            if result.grantedPermissions.contains("email")
+            {
+                returnUserData()
+            }
+        }
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        FBSDKAccessToken.setCurrentAccessToken(nil)
+        FBSDKProfile.setCurrentProfile(nil)
+        print("User Logged Out")
     }
 
+    func returnUserData()
+    {
+        let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "me", parameters: nil)
+        graphRequest.startWithCompletionHandler({ (connection, result, error) -> Void in
+            
+            if ((error) != nil)
+            {
+                // Process error
+                print("Error: \(error)")
+            }
+            else
+            {
+                
+                print("fetched user: \(result)")
+                let userName : NSString = result.valueForKey("name") as! NSString
+                print("User Name is: \(userName)")
+                let userID = result.valueForKey("id") as! NSString
+                let facebookProfileUrl = NSURL(string: "https://graph.facebook.com/\(userID)/picture?type=large")
+                
+                if let data = NSData(contentsOfURL: facebookProfileUrl!) {
+          
+                    let image = UIImage(data : data)
+                    self.profileImage.image = image
+                    self.lblName.text = "Welcome back " + ((userName) as String)               }
+            
+            }
+        })
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
